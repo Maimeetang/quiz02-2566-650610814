54521d3b75e7945bd7a61d2bfd113aff75dd6b40
Maimeetang <sirawit.36298@gmail.com>
2023-08-10 15:14:01 +0700

    completed

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,3 +1,5 @@
+import { Reply } from "./Reply";
+
 export const Comment = ({
   userImagePath,
   username,
@@ -5,10 +7,73 @@ export const Comment = ({
   likeNum,
   replies,
 }) => {
-  return (
-    <div>
-      your code here...
-      {/* map-loop render Reply component here */}
-    </div>
-  );
+  if (likeNum == 0) {
+    return (
+      <div>
+        <div className="d-flex gap-2 my-2">
+          <img
+            src={userImagePath}
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#3A3B3C" }}
+          >
+            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+              {username}
+            </span>
+            <br />
+            <span style={{ color: "#E4E6EB" }}>{commentText}</span>
+          </div>
+        </div>
+        {replies.map((replies) => (
+          <Reply
+            username={replies.username}
+            userImagePath={replies.userImagePath}
+            replyText={replies.replyText}
+            likeNum={replies.likeNum}
+          />
+        ))}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <div className="d-flex gap-2 my-2">
+          <img
+            src={userImagePath}
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#3A3B3C" }}
+          >
+            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+              {username}
+            </span>
+            <br />
+            <span style={{ color: "#E4E6EB" }}>{commentText}</span>
+            <div className={"d-flex align-items-center gap-1"}>
+              <img src="/like.svg" width={20}></img>
+              <span style={{ color: "#B0B3B8" }}>{likeNum} คน</span>
+            </div>
+          </div>
+        </div>
+        {replies.map((replies) => (
+          <Reply
+            username={replies.username}
+            userImagePath={replies.userImagePath}
+            replyText={replies.replyText}
+            likeNum={replies.likeNum}
+          />
+        ))}
+      </div>
+    );
+  }
 };
